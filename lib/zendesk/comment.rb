a9f33005151da7829efe85132302fd1f43d80b2b
Andrey Deryabin <deriabin@gmail.com>
2011-05-04 18:25:11 +0400

    save ticket

---
+++ lib/zendesk/comment.rb
@@ -0,0 +1,6 @@
+class Zendesk::Comment < Zendesk::Resource
+  
+  def initialize(value, is_public = true)
+    
+  end
+end
--- lib/zendesk/resource.rb
+++ lib/zendesk/resource.rb
@@ -1,44 +1,75 @@
 require 'rest_client'
 class Zendesk::Resource
   include ActiveSupport::Callbacks
-  
-  define_callbacks :before_save, :after_save
+
 
   CONFIG = YAML.load_file(ENV['CONFIG'] || 'config.yml')
   RESOURCE = RestClient::Resource.new CONFIG['host'], :user => CONFIG['user'], :password => CONFIG['password'], :timeout => 20, :open_timeout => 1
 
-  @@attributes = {}
-  @@attribute_methods = ()
+  attr_reader :data, :attributes, :protected_attributes
 
   def initialize(attributes = {})
-    attr_keys = CONFIG[self.class.to_s.demodulize.downcase]
-    
-    @fields = {}
-    attr_keys.try(:keys).each do |attr_name|
-      @fields[attr_name.to_sym]  = attr_keys[attr_name]
+    load_accessors(attributes)
+  end
+
+  def self.find(id)
+    begin
+      data = load_data(RESOURCE["#{self.to_s.demodulize.downcase.pluralize}/#{id}.json"].get)
+      new_object = self.new(data)
+      new_object.load_protected_attributes(data)
+      new_object
+    rescue
+      nil
     end
-    
-    @attributes = {}
-    @@attributes.keys.each do |attr_name|
-      @attributes[attr_name.to_sym]  = attributes[attr_name.to_sym] || attributes[@@attributes[attr_name.to_sym]]
-    end
-    
-    @@attribute_methods.each do |name|
-      self.class.send(:define_method, "#{name}=".to_sym) { |val| @attributes[name.to_sym] = val }
-      self.class.send(:define_method, name.to_sym) { instance_variable_get("@attributes")[name.to_sym] }
-    end
+  end
+
+
+  def self.attr_accessor(*vars)
+    @attributes ||= []
+    @attributes.concat vars
+    super(*vars)
+  end
+
+  def self.attr_reader(*vars)
+    @protected_attributes ||= []
+    @protected_attributes.concat vars
+    super(*vars)
   end
   
-  def self.find(id)
-    RESOURCE["#{self.to_s.demodulize.downcase.pluralize}/#{id}.json"].get
+  
+  def self.attributes
+    @attributes
   end
-    
-  def load_data(json)
-    data  =JSON(json)
+  
+  def attributes
+    self.class.attributes
   end
-  # def save
-  #   run_callbacks(:before_save)
-  #   super
-  #   run_callbacks(:after_save)
-  # end
+
+  def self.protected_attributes
+    @protected_attributes
+  end
+
+  
+  def protected_attributes
+    self.class.protected_attributes
+  end
+
+
+  def self.load_data(json_data)
+    puts JSON(json_data).inspect
+    JSON(json_data)
+  end
+
+  def load_accessors(attrs = {})
+    attributes.each do |attr|
+      instance_variable_set("@#{attr}", attrs[attr.to_s])
+    end
+  end
+
+  def load_protected_attributes(attrs = {})
+    protected_attributes.each do |attr|
+      instance_variable_set("@#{attr}", attrs[attr.to_s])
+    end
+  end
+
 end
